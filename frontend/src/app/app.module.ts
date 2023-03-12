ff0ea503f653d061012da9af01855a094227158d
Sofia Reis <57951365+mestrecalendo@users.noreply.github.com>
2023-03-11 23:02:12 -0400

    Adicionado estilo dos componentes cabeçalho e formulário

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './componentes/formulario/formulario.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    CabecalhoComponent
   ],
   imports: [
     BrowserModule,
